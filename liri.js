23bd3aea125a0f5b98a326bf0ad641432891247f
Kevin Lawler <kevinrlawler@gmail.com>
2017-02-07 20:19:03 -0500

    fixed the twitter call to pull up past tweets

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
-var twitter = require("twitter");
+var Twitter = require("twitter");
 
-var client = new twitter({
+var client = new Twitter({
   consumer_key: 'M389Ck4WpwNHQ2Mlwlb1NOUsN',
   consumer_secret: 'dCAUfWvSNH6xmw75JipBB6yUq3M9L4cJ3stuwNSICPti80s4Ox',
   access_token_key: '297511027-OklWkTt224FdN43jtvkYvBX9qRTrqFZVvR9XXm3S',
@@ -10,18 +10,39 @@ var client = new twitter({
 var myTweets = process.argv[2];
 
 // var params = {screen_name: 'klaw1387'};
-client.get('statuses/user_timeline', myTweets, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
-
-twitter.writeFile("log.txt", "Hey there!", function(err) {
-	if(!err) {
-		console.log("The file was saved!")
-	}
-})
-
-// client.get('search/tweets', {q: '@klaw1387'}, function(error, tweets, response) {
-//    console.log(tweets);
+// client.get('statuses/user_timeline', myTweets, function(error, tweets, response) {
+//   if (!error) {
+//     console.log(tweets);
+//   }
 // });
+
+// twitter.writeFile("log.txt", "Hey there!", function(err) {
+// 	if(!err) {
+// 		console.log("The file was saved!")
+// 	}
+// })
+// }
+
+
+
+var getMyTweets = function() {
+  // var client = Twitter(keys.twitterKeys)
+
+  var params = {
+    screen_name: "klaw1387" 
+  };
+
+  client.get("statuses/user_timeline", params, function(error, tweets, response) {
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log("");
+        console.log(tweets[i].text);
+      }
+    }
+  });
+};
+
+getMyTweets();
+
+
